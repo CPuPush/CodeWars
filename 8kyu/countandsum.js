9e6057037396917c269a47658a14c0a2f81dc2ca
CPuPush <fpakpahan44@gmail.com>
2022-05-11 16:42:07 +0700

    add countandsum

--- 8kyu/countandsum.js
+++ 8kyu/countandsum.js
@@ -1,20 +1,25 @@
+//solution 1
 function countPositivesSumNegatives(input) {
-    let possitive = 0;
-    let negative = 0;
-    if(!input.length||!input){
+    if(!input || !input.length){
         return [];
     }
-    for(let i=0;i<input.length;i++){
-        if(input[i] > 0){
-            possitive+=1;
-        }else if(input[i]<0){
-            negative+=input[i];
-        }
-    }
-    // if(possitive==0&&negative==0){
-    //     return [];
-    // }
-    return [possitive, negative];
+    const result = [0, 0];
+    input.forEach((x)=>{
+        x > 0 ? result[0]++ : (result[1]+=x);
+    })
+    return result;
 }
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
 console.log(countPositivesSumNegatives([]));
+
+//solution 2
+function countPositivesSumNegatives2(input) {
+    return input && input.length
+        ? [
+            input.filter((p)=>p > 0).length,
+            input.filter((p)=>p < 0).reduce((a, b)=>a+b,0)
+        ]
+        : []
+}
+console.log(countPositivesSumNegatives2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+console.log(countPositivesSumNegatives2([]));
