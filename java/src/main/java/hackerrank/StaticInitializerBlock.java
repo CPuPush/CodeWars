c5df3143b18648e238fa2af310d3bde86958c311
CPuPush <fpakpahan44@gmail.com>
2024-07-23 14:20:08 +0700

    2 for today

--- java/src/main/java/hackerrank/StaticInitializerBlock.java
+++ java/src/main/java/hackerrank/StaticInitializerBlock.java
@@ -3,10 +3,31 @@ package hackerrank;
 import java.util.Scanner;
 
 public class StaticInitializerBlock {
+
+    public static int n;
+    public static int k;
+    public static boolean flag;
     public static void main(String[] args) {
+        if(flag){
+            int area = n * k;
+            System.out.println(area);
+        }
+    }
+    static{
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        if (n < 1 || k == 0) {}
+        n=in.nextInt();
+        k=in.nextInt();
+
+        if(n <=0 || k<=0){
+            flag=false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }else {
+            flag = true;
+        }
     }
 }
+/*
+! Static Initialization Blocks
+This is especially useful if subclasses might want to reuse the initialization method
+static dapat memanggil class static yang lain
+* */
